62d4c6ad12073895ca2b8db7a11102367f0b09ca
Luiza-Camargo <luiza.camargo@euax.com.br>
2022-12-24 13:06:38 -0300

    Alterando as pesquisas para scopes

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,11 +5,6 @@ class Question < ApplicationRecord
 
   paginates_per 5
 
-  def self.search(page, term)
-    Question.includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page)
-  end
-
-  def self.last_questions(page)
-    Question.includes(:answers).order('created_at desc').page(page)
-  end
+  scope :_search_, ->(page, term) { Question.includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page) }
+  scope :last_questions, ->(page) { Question.includes(:answers).order('created_at desc').page(page) }
 end
